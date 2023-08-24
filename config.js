817758ce9f390fec4799c75835535915220cacb8
DeMgJE <1434628640@qq.com>
2023-08-24 16:48:34 +0800

    read config from process.env

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 const config = {
-    TOKEN: "xoxc-xxxx",
-    COOKIE: "xoxd-xxxx",
-    TEAM_ID: "workspacename",
-    CHANNEL: "C1234D4567S",
-    CLAUDE_USER: "U1234A568BC",
+    TOKEN: process.env.TOKEN,
+    COOKIE: process.env.COOKIE,
+    TEAM_ID: process.env.TEAM_ID,
+    CHANNEL: process.env.CHANNEL,
+    CLAUDE_USER: process.env.CLAUDE_USER,
 
-    MAINPROMPT_LAST: false, // Will try to move the main prompt (main + nsfw prompt from SillyTavern) to the bottom, similar to a jailbreak
-    MAINPROMPT_AS_PING: false, // Will use the main prompt as the ping message, if this true the PING_MESSAGE setting is ignored. Overrides MAINPROMPT_LAST.
+    MAINPROMPT_LAST: process.env.MAINPROMPT_LAST  === 'true', // Will try to move the main prompt (main + nsfw prompt from SillyTavern) to the bottom, similar to a jailbreak
+    MAINPROMPT_AS_PING: process.env.MAINPROMPT_AS_PING  === 'true', // Will use the main prompt as the ping message, if this true the PING_MESSAGE setting is ignored. Overrides MAINPROMPT_LAST.
     
     // If false will make Slaude send messages as plain text rather than through Slack's blocks format. The results in Slack should be the same either way.
-    USE_BLOCKS: true,
+    USE_BLOCKS: process.env.USE_BLOCKS  === 'true',
 
     STREAMING_TIMEOUT: 240000, //Time in milliseconds after which the response stream will force close itself, 0 disables the timeout but is not recommended.
 
@@ -17,7 +17,7 @@ const config = {
     // If there is no @Claude, the ping will automatically get added in front of the message.
     // Can be whatever you want it to be but keep in mind that it might result in unpredictable responses.
     // Anything we put here will eat into our max available tokens so keep it brief.
-    PING_MESSAGE: "Assistant:",
+    PING_MESSAGE: process.env.PING_MESSAGE,
 
     PORT: 5004
 }
